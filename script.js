7517a412379a58557b460cbacafc2547301bbcee
Dexter Pai <dr.pyiephonepai@gmail.com>
2023-12-22 18:30:37 -0600

    update: best out of 5 games added

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const prompt = require('prompt-sync')({sigint: true});
 
 let choices = ['rock', 'paper', 'scissor'];
+let scores = {
+  player: 0,
+  npc: 0
+}
 
 function main() {
 
@@ -24,14 +28,30 @@ function main() {
 
   console.log('\n');
 
-  let template = `Player choose ${choices[playerChoice]} and NPC choose ${choices[npcChoice]}`;
+  let template = `Player chooses ${choices[playerChoice]} and NPC choose ${choices[npcChoice]}`;
 
   if (playerChoice > npcChoice || (playerChoice === 3 && npcChoice === 0)) {
     console.log('Player Wins! ' + template);
+    scores.player++;
   } else if (npcChoice > playerChoice || (npcChoice === 3 && playerChoice === 0)) {
     console.log('Player Loses! ' + template);
+    scores.npc++;
   } else {
     console.log("It's a draw!")
+  }
+  // debug
+  // console.log(scores.player);
+  // console.log(scores.npc);
+
+  switch(5) {
+    case (scores.player):
+      console.log('You win the game!');
+      return 0;
+      break;
+    case (scores.npc):
+      console.log('You lose!');
+      return 0;
+      break;
   }
   console.log('\n')
   let rematch;
